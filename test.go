42d6ef18b44c92d3bbed0b006c46314c698f282e
Your Name <you@example.com>
2024-11-19 19:41:35 +0530

    static and dynamic variable declaration

--- test.go
+++ test.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Printf("hello, businessman msksurjith\n")
-
+	// static declaration variable
 	var num1 int
 	num1 = 100
 	fmt.Printf("%d\n", num1)
@@ -12,5 +12,15 @@ func main() {
 	var floatvalue float32
 	floatvalue = 100.1
 	fmt.Printf("Value of num1 is: %d\n", num1)
-	fmt.Printf("Value of Float is: %f", floatvalue)
+	fmt.Printf("Value of Float is: %f\n", floatvalue)
+
+	// dynamic declaration variable
+
+	x := 1000
+	y := 10.22
+	name := "msk"
+	fmt.Printf("%d\n", x)
+	fmt.Printf("%f\n", y)
+	fmt.Printf("%s\n", name)
+
 }
