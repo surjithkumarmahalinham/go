f5d868d8e5d148d73c7991d9c8ff2dc7ee4580ef
Your Name <you@example.com>
2024-11-20 19:47:25 +0530

    switch case and looping functionality

---
+++ switch.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Printf("vale")
+
+	value := 3
+
+	switch value {
+	case 1:
+		fmt.Printf("One")
+	case 2:
+		fmt.Printf("Two")
+	case 3:
+		fmt.Printf("Three")
+	case 4:
+		fmt.Printf("Four")
+	}
+
+	for a := 0; a <= 10; a++ {
+		fmt.Printf("Hi Business Man Msksurjith \n")
+	}
+
+	m := 5
+	for m < 10 {
+		fmt.Printf("How are you Msksurjith \n")
+		m++
+	}
+	fmt.Printf("\nProgram finished")
+
+}
