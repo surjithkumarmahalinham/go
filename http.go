b7549bef66c388ed5c736d1f673af6fe8291b2e3
Your Name <msksurjith@gmail.com>
2024-12-10 15:17:51 +0530

    html tag with content type

--- http.go
+++ http.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	myHandler := func(w http.ResponseWriter, r *http.Request) {
-
+		w.Header().Set("Content-Type", "text/html")
 		fmt.Fprintf(w, "<h1>Hellow World !</h1>")
 	}
 
